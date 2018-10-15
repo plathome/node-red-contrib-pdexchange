056d3b74e86e83d95b1d6506876bdd56dbeb124b
Masato Minda <belgianbeer@aj.admwt.jp>
2018-10-15 13:55:45 +0900

    indent correction

--- 1-pdex-send.js
+++ 1-pdex-send.js
@@ -1,50 +1,50 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
 
 	function jsonparse(jstr) {
 		try {
 			var out = JSON.parse(jstr);
 			return out;
 		}
-		catch(e) {
+		catch (e) {
 			return "";
 		}
 	}
 
-    function PdexRESTPublishNode(config) {
+	function PdexRESTPublishNode(config) {
 		var request = require('request');
 
-    	var hmacUri = '/api/v1/utils/hmac';
-    	var deviceMsgSendUri = '/api/v1/devices/';
+		var hmacUri = '/api/v1/utils/hmac';
+		var deviceMsgSendUri = '/api/v1/devices/';
 
-        RED.nodes.createNode(this, config);
-        var node = this;
+		RED.nodes.createNode(this, config);
+		var node = this;
 
-	    node.on('input', function(msg) {
-	    	var pdexurl = config.pdexurl;
-	    	var accesskey = config.accesskey;
-	    	var deviceid = config.deviceid;
-	    	var secretkey = config.secretkey;
-	    	var appid = config.appid;
-	    	var apptoken = config.apptoken;
+		node.on('input', function (msg) {
+			var pdexurl = config.pdexurl;
+			var accesskey = config.accesskey;
+			var deviceid = config.deviceid;
+			var secretkey = config.secretkey;
+			var appid = config.appid;
+			var apptoken = config.apptoken;
 
-	    	var payloadObject = msg.payload;
+			var payloadObject = msg.payload;
 
 			var options_hmac = {
-			  uri: pdexurl + hmacUri,
-			  headers: {
-			    "Content-type": "application/json",
-			  },
-			  json: {
-			    "key": secretkey,
-			    "message": deviceid,
-			    "eq_stripped": true
-			  }
+				uri: pdexurl + hmacUri,
+				headers: {
+					"Content-type": "application/json",
+				},
+				json: {
+					"key": secretkey,
+					"message": deviceid,
+					"eq_stripped": true
+				}
 			};
 
-			request.post(options_hmac, function(error, response, body){
+			request.post(options_hmac, function (error, response, body) {
 				if (error) {
-                    node.error(error);
-                    node.status({fill:"red", shape:"ring", text:"pdex hmac create failed"});
+					node.error(error);
+					node.status({ fill: "red", shape: "ring", text: "pdex hmac create failed" });
 					return console.log(error);
 				}
 
@@ -52,18 +52,18 @@ module.exports = function(RED) {
 
 				if (code == 200) {
 					var options_send = {
-					  uri: pdexurl + deviceMsgSendUri + '/' + deviceid + '/message',
-					  headers: {
-					    "Content-type": "application/json",
-						'Authorization': 'Bearer ' + body.digest
-					  },
-					  json: { payloadObject }
+						uri: pdexurl + deviceMsgSendUri + '/' + deviceid + '/message',
+						headers: {
+							"Content-type": "application/json",
+							'Authorization': 'Bearer ' + body.digest
+						},
+						json: { payloadObject }
 					};
 
-					request.post(options_send, function(error, response, body){
+					request.post(options_send, function (error, response, body) {
 						if (error) {
-		                    node.error(error);
-		                    node.status({fill:"red", shape:"ring", text:"pdex data send failed"});
+							node.error(error);
+							node.status({ fill: "red", shape: "ring", text: "pdex data send failed" });
 							return console.log(error);
 						}
 
@@ -71,8 +71,8 @@ module.exports = function(RED) {
 
 						if (code == 201) {
 							var msg = {
-								payload : jsonparse(payloadObject),
-								transaction : jsonparse(body)
+								payload: jsonparse(payloadObject),
+								transaction: jsonparse(body)
 							};
 							node.send(msg);
 							node.status({});
@@ -80,7 +80,7 @@ module.exports = function(RED) {
 					});
 				}
 			});
-        });
-    }
-    RED.nodes.registerType("PD Exchange",PdexRESTPublishNode);
+		});
+	}
+	RED.nodes.registerType("PD Exchange", PdexRESTPublishNode);
 }
